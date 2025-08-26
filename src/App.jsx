4f41a14c83ac267b56fbb2dbab4709c60c61a45e
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2025-08-26 23:39:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ const App = () => {
     <div className='' >
     <Header/>
     <Mainpage/>
-       <p className='text-base lg:text-2xl md:text-lg text-blue-600 font-raw py-2 my-4 px-3 scale-75 sm:scale-90'>
+       <p className='text-base lg:text-3xl md:text-lg text-white-800 font-raw py-2 my-4 px-3 scale-95 sm:scale-90'>
 Driven, detail-oriented full-stack developer with strong foundations in AI, ML, and modern web technologies.
 Passionate about building intelligent, scalable solutions while maintaining precision and quality. </p>
     <Page2/>
-         <p className='text-base lg:text-2xl md:text-lg text-blue-600 font-raw py-2 my-4 px-3 scale-75 sm:scale-90'>
+        <p className='text-base lg:text-3xl md:text-lg text-white-800 font-raw py-2 my-4 px-3 scale-95 sm:scale-90'>
 "Worked on AI & Machine Learning projects involving NLP, deep learning model development, and predictive analytics. Designed and deployed containerized workflows
  using Docker, enabling scalable, efficient, and production-ready AI solutions </p>
     {/* <CertificationsPage/> */}
@@ -25,23 +25,23 @@ Passionate about building intelligent, scalable solutions while maintaining prec
     
     <Page4/>
 
-      <p className='text-base lg:text-2xl md:text-lg text-blue-500 font-raw py-2 mt-8 px-3 scale-75 sm:scale-90'>
+     <p className='text-base lg:text-3xl md:text-lg text-white-800 font-raw py-2 my-4 px-3 scale-95 sm:scale-90'>
 Achievements & Milestones
 Recognized for excellence in academics, technical innovation, and collaborative projects.
 Dedicated to continuous growth through challenges, competitions, and impactful contributions. </p>
     <Page3/>
 
-      <p className='text-base lg:text-2xl md:text-lg text-blue-500 font-raw py-2 mt-8 px-3 scale-75 sm:scale-90'>
+     <p className='text-base lg:text-3xl md:text-lg text-white-800 font-raw py-2 my-4 px-3 scale-95 sm:scale-90'>
 Passionate full-stack developer with a strong foundation in AI, ML, and modern web technologies.
 Focused on building intelligent, scalable, and user-friendly solutions with precision and creativity. </p>
     <Page5/>
 
-      <p className='text-base lg:text-2xl md:text-lg text-blue-500 font-raw py-2 my-16 px-3 scale-75 sm:scale-90'>
-Contact
-Let's connect to collaborate, innovate, and create impactful solutions.
-Reach out via phone, email, or social platforms to start the conversation. </p>
+      <p className='text-base lg:text-3xl md:text-lg text-white-800 font-raw py-2 my-4 px-3 scale-95 sm:scale-90'>
+  Contact
+  Let's connect to collaborate, innovate, and create impactful solutions.
+  Reach out via phone, email, or social platforms to start the conversation. </p>
     <ContactUs/>
-    <p className='text-base lg:text-2xl md:text-lg text-blue-500 sm:text-blue-100 sm:scale-95 font-raw py-2 my-16 px-3 scale-75 sm:scale-90'>
+    <p className='text-base lg:text-3xl md:text-lg text-white-800 font-raw py-2 my-4 px-3 scale-95 sm:scale-90'>
 Thank you for taking the time to explore my portfolio.
 I hope this journey through my work, skills, and achievements has given you a glimpse of my passion for technology and innovation.
 Your visit means a lot — and I look forward to potential collaborations, new ideas, and exciting opportunities ahead. </p>
